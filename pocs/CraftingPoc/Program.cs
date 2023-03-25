e4960e24394332ad9f164f4596938772e533683c
Brent Acs <brent.mann@gmail.com>
2023-03-25 06:30:59 -0400

    WIP

--- pocs/CraftingPoc/Program.cs
+++ pocs/CraftingPoc/Program.cs
@@ -2,8 +2,49 @@
 Console.WriteLine("Hello, World!");
 
 
+/*
 
-public interface IRawResource
+   Raw Resource -> Refined Resource -> Product 
+                                    -> Component -> Product 
+                                                 -> Complex Component -> Product
+ */
+
+
+
+public interface ICraftingEntity
 {
-
+   int Id { get; }
 }
+
+public class RawResource : ICraftingEntity
+{
+   public int Id { get; set; }
+   public string Name { get; set; } = string.Empty;
+}
+
+
+public abstract class BasePocRepo
+{
+   private int _nextId = 0;
+
+   protected int NextId => _nextId++;
+}
+
+public class RawResourceRepo : BasePocRepo
+{
+   private IList<RawResource> _resources = new List<RawResource>();
+
+   public RawResourceRepo()
+   {
+      Add("Copper Ore");      
+      Add("Iron Ore");
+      Add("Coal");    
+   }
+   
+   public IEnumerable<RawResource> RawResources => _resources;
+
+   public void Add(string name) =>
+      _resources.Add(new RawResource{Id = NextId, Name = name});
+}
+
+
