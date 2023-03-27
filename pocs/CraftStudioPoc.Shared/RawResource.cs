000833df961de808eef2f02dd827edf6c28637ad
Brent Acs <brent.mann@gmail.com>
2023-03-26 20:08:01 -0400

    WIP

---
+++ pocs/CraftStudioPoc.Shared/RawResource.cs
@@ -0,0 +1,32 @@
+﻿namespace CraftStudioPoc.Shared;
+
+public class RawResource : ICraftingEntity
+{
+   public int Id { get; set; }
+   public string Name { get; set; } = string.Empty;
+}
+
+public interface IRawResourceCraftingTable : ICraftingTable<RawResource>
+{
+}
+
+public class RawResourceCraftingTable : CraftingTable<RawResource>, IRawResourceCraftingTable
+{
+   public RawResourceCraftingTable()
+   {
+      Add("Copper Ore");
+      Add("Iron Ore");
+      Add("Coal");
+   }
+
+   private void Add(string name) =>
+      Add(new RawResource { Name = name });
+}
+
+
+public class RefinedResource : ICraftingEntity
+{
+   public int Id { get; set; }
+   public string Name { get; set; } = string.Empty;
+
+}
--- tests/PocTests/UnitTest1.cs
+++ tests/PocTests/UnitTest1.cs
@@ -1,9 +1,18 @@
+using FluentAssertions;
+
 namespace PocTests;
 
 public class UnitTest1
 {
+   public class TestCraftingTable : CraftingTable<RawResource>
+   {
+   }
+   
    [Fact]
    public void Test1()
    {
+      var sut = new TestCraftingTable();
+
+      sut.Items.Should().BeEmpty();
    }
 }
